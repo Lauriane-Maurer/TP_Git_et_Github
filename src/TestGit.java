f75dbbe3e886395dd47b64da0c5a843810e01306
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-03-21 15:41:39 +0100

    Ajout constructeur classe TestGit

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,4 +1,10 @@
 public class TestGit {
 
+    private String nom;
+    private String prenom;
 
-}
+    public TestGit(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
