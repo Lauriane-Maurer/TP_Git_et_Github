5bf9fc3cf909f082077a3602bc672f1812779ca5
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-03-21 15:51:10 +0100

    Ajout getter

--- src/TestGit.java
+++ src/TestGit.java
@@ -8,3 +8,11 @@ public class TestGit {
         this.prenom = prenom;
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+}
